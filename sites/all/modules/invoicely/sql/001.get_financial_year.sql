09a95e924fda521e122c743d9cb74aef81e284b0
Matthew Baggett <matthew@baggett.me>
2013-10-12 00:52:33 +0100

    I already hate postgres.

--- sites/all/modules/invoicely/sql/001.get_financial_year.sql
+++ sites/all/modules/invoicely/sql/001.get_financial_year.sql
@@ -1,13 +1,12 @@
 DROP FUNCTION IF EXISTS get_financial_year();
 
 CREATE FUNCTION get_financial_year (date_given DATETIME)
-RETURNS VARCHAR(9) DETERMINISTIC
-RETURN 
+RETURNS VARCHAR(9)
+AS '
 		CASE 
-			WHEN MONTH(date_given)>=4 
-		THEN
-			concat(YEAR(date_given), '-',YEAR(date_given)+1)
-		ELSE 
-			concat(YEAR(date_given)-1,'-', YEAR(date_given))
-		END;
-		
+			WHEN MONTH(date_given)>=4 THEN
+			  RETURN concat(YEAR(date_given), "-", YEAR(date_given)+1);
+		  ELSE
+			  RETURN concat(YEAR(date_given)-1, "-", YEAR(date_given));
+		END CASE;
+  '
