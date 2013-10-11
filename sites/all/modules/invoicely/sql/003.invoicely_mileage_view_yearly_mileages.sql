fa83510c1e51ff6b713050b141fc472517931abc
Matthew Baggett <matthew@baggett.me>
2013-10-11 23:16:38 +0100

    Rename the files into execution order, yearly_mileages had bad SQL.

---
+++ sites/all/modules/invoicely/sql/003.invoicely_mileage_view_yearly_mileages.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE VIEW  `invoicely_mileage_view_yearly_mileages` AS
+SELECT
+  `imv`.`uid` AS `uid`,
+  `imv`.`username` AS `username`,
+  `imv`.`year` AS `year`,
+  `imv`.`financial_year` AS `financial_year`,
+  sum(`imv`.`distance`) AS `distance`,
+  if((sum(`imv`.`distance`) > 10000),10000,sum(`imv`.`distance`)) AS `distance_high_rate`,
+  if((sum(`imv`.`distance`) > 10000),(sum(`imv`.`distance`) - 10000),0) AS `distance_low_rate`
+FROM
+  `invoicely_mileage_view` `imv`
+GROUP BY
+  `imv`.`financial_year`
--- sites/all/modules/invoicely/sql/invoicely_mileage_view_yearly_mileages.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE VIEW invoicely_mileage_view_yearly_amounts AS
-SELECT 
-	imvym.`username`,
-	imvym.`financial_year`,
-	SUM(imvym.`distance`) as `distance`,
-	(imvym.distance_high_rate * 0.45) + (distance_low_rate * 0.20) as `amount`
-
-FROM invoicely_mileage_view_yearly_mileages imvym
-GROUP BY imvym.`financial_year`
