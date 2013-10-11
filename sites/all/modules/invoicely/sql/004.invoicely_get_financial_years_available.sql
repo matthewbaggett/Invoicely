10dd480148a7d45c720fb0e313f6d1905a3a0cf5
Matthew Baggett <matthew@baggett.me>
2013-10-11 23:23:28 +0100

    That view don't exist...

--- sites/all/modules/invoicely/sql/004.invoicely_get_financial_years_available.sql
+++ sites/all/modules/invoicely/sql/004.invoicely_get_financial_years_available.sql
@@ -2,5 +2,5 @@ CREATE OR REPLACE VIEW invoicely_get_financial_years_available AS
 SELECT 
 	iivmt.`financial_year`,
 	iivmt.`uid`
-FROM invoicely_invoice_view_monthly_totals iivmt
+FROM invoicely_invoices_view_monthly_totals iivmt
 GROUP BY iivmt.`financial_year`
